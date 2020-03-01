db1749b562fbf32cb6807979d1095b0ba714ccc1
Eric Tao <twqzy323@163.com>
2020-03-01 23:50:04 +0800

    fix import path incorrect problem

--- phpstruct.go
+++ phpstruct.go
@@ -1,289 +1,287 @@
 package toml2php
 
 import (
-    "bytes"
-    "strings"
+	"bytes"
+	"strings"
 
-    "toml2php/util"
+	"github.com/whencome/toml2php/util"
 )
 
 // define php data type
 const (
-    PhpTypeNumber = iota
-    PhpTypeBoolean
-    PhpTypeString
-    PhpTypeValue
-    PhpTypeArray
+	PhpTypeNumber = iota
+	PhpTypeBoolean
+	PhpTypeString
+	PhpTypeValue
+	PhpTypeArray
 )
 
 // define indent string, default 4 whitespace
 var IndentString = "    "
+
 // define php array format, use "array()" or "[]"
 var PHPArrayStartString = "array("
 var PHPArrayEndString = ")"
 
 // PHPValue define a php value
 type PHPValue struct {
-    Value       interface{}
-    Type        int  // indicate the value type, can be int,float,string,bool,array
+	Value interface{}
+	Type  int // indicate the value type, can be int,float,string,bool,array
 }
 
 type PHPKey struct {
-    Value       string
-    IsNumeric   bool  // 键值是否是数字
+	Value     string
+	IsNumeric bool // 键值是否是数字
 }
 
 type PHPKeyValuePair struct {
-    Key         string
-    Value       interface{}
-    Type        int
+	Key   string
+	Value interface{}
+	Type  int
 }
 
 // PHPArray define a php array （array & map）
 type PHPArray struct {
-    Values          []*PHPKeyValuePair
+	Values []*PHPKeyValuePair
 }
 
 func NewPHPArray() *PHPArray {
-    return &PHPArray{
-        Values:make([]*PHPKeyValuePair, 0),
-    }
+	return &PHPArray{
+		Values: make([]*PHPKeyValuePair, 0),
+	}
 }
 
 // 定义一个数组值
 func NewPHPValue() *PHPValue {
-    return &PHPValue{}
+	return &PHPValue{}
 }
 
 // NewPHPBoolValue create a PHP boolean value
 func NewPHPBoolValue(val string) *PHPValue {
-    return &PHPValue{
-        Value:val,
-        Type:PhpTypeBoolean,
-    }
+	return &PHPValue{
+		Value: val,
+		Type:  PhpTypeBoolean,
+	}
 }
 
 // NewPHPNumberValue create a PHP number value
 func NewPHPNumberValue(val string) *PHPValue {
-    return &PHPValue{
-        Value:val,
-        Type:PhpTypeNumber,
-    }
+	return &PHPValue{
+		Value: val,
+		Type:  PhpTypeNumber,
+	}
 }
 
 // NewPHPStringValue create a PHP boolean value
 func NewPHPStringValue(val string) *PHPValue {
-    return &PHPValue{
-        Value:val,
-        Type:PhpTypeString,
-    }
+	return &PHPValue{
+		Value: val,
+		Type:  PhpTypeString,
+	}
 }
 
 // NewPHPArrayValue create a PHP boolean value
 func NewPHPArrayValue(val *PHPArray) *PHPValue {
-    return &PHPValue{
-        Value:val,
-        Type:PhpTypeArray,
-    }
+	return &PHPValue{
+		Value: val,
+		Type:  PhpTypeArray,
+	}
 }
 
 // String format PHPValue as php code
 func (phpVal *PHPValue) String(depth int) string {
-    switch phpVal.Type {
-    case PhpTypeBoolean:
-        return util.NewValue(phpVal.Value).String()
-    case PhpTypeNumber:
-        return util.NewValue(phpVal.Value).String()
-    case PhpTypeString:
-        return fmtPhpString(util.NewValue(phpVal.Value).String())
-    case PhpTypeArray:
-        phpArr := phpVal.Value.(*PHPArray)
-        return phpArr.String(depth)
-    case PhpTypeValue:
-        phpVal := phpVal.Value.(*PHPValue)
-        return phpVal.String(depth)
-    }
-    return ""
+	switch phpVal.Type {
+	case PhpTypeBoolean:
+		return util.NewValue(phpVal.Value).String()
+	case PhpTypeNumber:
+		return util.NewValue(phpVal.Value).String()
+	case PhpTypeString:
+		return fmtPhpString(util.NewValue(phpVal.Value).String())
+	case PhpTypeArray:
+		phpArr := phpVal.Value.(*PHPArray)
+		return phpArr.String(depth)
+	case PhpTypeValue:
+		phpVal := phpVal.Value.(*PHPValue)
+		return phpVal.String(depth)
+	}
+	return ""
 }
 
 func (phpKV *PHPKeyValuePair) GetValue(depth int) string {
-    switch phpKV.Type {
-    case PhpTypeBoolean:
-        return util.NewValue(phpKV.Value).String()
-    case PhpTypeNumber:
-        return util.NewValue(phpKV.Value).String()
-    case PhpTypeString:
-        return fmtPhpString(util.NewValue(phpKV.Value).String())
-    case PhpTypeArray:
-        phpArr := phpKV.Value.(*PHPArray)
-        return phpArr.String(depth)
-    case PhpTypeValue:
-        phpVal := phpKV.Value.(*PHPValue)
-        return phpVal.String(depth)
-    }
-    return ""
+	switch phpKV.Type {
+	case PhpTypeBoolean:
+		return util.NewValue(phpKV.Value).String()
+	case PhpTypeNumber:
+		return util.NewValue(phpKV.Value).String()
+	case PhpTypeString:
+		return fmtPhpString(util.NewValue(phpKV.Value).String())
+	case PhpTypeArray:
+		phpArr := phpKV.Value.(*PHPArray)
+		return phpArr.String(depth)
+	case PhpTypeValue:
+		phpVal := phpKV.Value.(*PHPValue)
+		return phpVal.String(depth)
+	}
+	return ""
 }
 
 func (phpKV *PHPKeyValuePair) String(depth int) string {
-    buf := bytes.Buffer{}
-    buf.WriteString(strings.Repeat(IndentString, depth))
-    if isPositiveIntNumeric(phpKV.Key) {
-        buf.WriteString(phpKV.Key)
-    } else {
-        buf.WriteString(fmtPhpString(phpKV.Key))
-    }
-    buf.WriteString(" => ")
-    buf.WriteString(phpKV.GetValue(depth))
-    return buf.String()
+	buf := bytes.Buffer{}
+	buf.WriteString(strings.Repeat(IndentString, depth))
+	if isPositiveIntNumeric(phpKV.Key) {
+		buf.WriteString(phpKV.Key)
+	} else {
+		buf.WriteString(fmtPhpString(phpKV.Key))
+	}
+	buf.WriteString(" => ")
+	buf.WriteString(phpKV.GetValue(depth))
+	return buf.String()
 }
 
 func NewNumberKey(v string) *PHPKey {
-    return &PHPKey{
-        Value:v,
-        IsNumeric:true,
-    }
+	return &PHPKey{
+		Value:     v,
+		IsNumeric: true,
+	}
 }
 
 func NewStringKey(v string) *PHPKey {
-    return &PHPKey{
-        Value:v,
-        IsNumeric:false,
-    }
+	return &PHPKey{
+		Value:     v,
+		IsNumeric: false,
+	}
 }
 
 func (phpKey *PHPKey) String() string {
-    if phpKey.IsNumeric {
-        return phpKey.Value
-    }
-    return fmtPhpString(phpKey.Value)
+	if phpKey.IsNumeric {
+		return phpKey.Value
+	}
+	return fmtPhpString(phpKey.Value)
 }
 
 // AddRecurseKeys add/initialize recursed keys
 func (phpArr *PHPArray) AddRecurseKeys(fields []string) {
-    fieldsSize := len(fields)
-    if fieldsSize == 0 {
-        return
-    }
-    refPhpArr := phpArr
-    for i := 0; i < fieldsSize; i++ {
-        field := fields[i]
-        found := false
-        for _, v := range refPhpArr.Values {
-            if v.Key == field && v.Type == PhpTypeArray {
-                refPhpArr = v.Value.(*PHPArray)
-                found = true
-            }
-        }
-        if !found {
-            arr := NewPHPArray()
-            kvPair := &PHPKeyValuePair{
-                Key:field,
-                Type:PhpTypeArray,
-                Value:arr,
-            }
-            refPhpArr.Values = append(refPhpArr.Values, kvPair)
-            refPhpArr = arr
-        }
-    }
+	fieldsSize := len(fields)
+	if fieldsSize == 0 {
+		return
+	}
+	refPhpArr := phpArr
+	for i := 0; i < fieldsSize; i++ {
+		field := fields[i]
+		found := false
+		for _, v := range refPhpArr.Values {
+			if v.Key == field && v.Type == PhpTypeArray {
+				refPhpArr = v.Value.(*PHPArray)
+				found = true
+			}
+		}
+		if !found {
+			arr := NewPHPArray()
+			kvPair := &PHPKeyValuePair{
+				Key:   field,
+				Type:  PhpTypeArray,
+				Value: arr,
+			}
+			refPhpArr.Values = append(refPhpArr.Values, kvPair)
+			refPhpArr = arr
+		}
+	}
 }
 
 // AddDeepValue add value for specified path, which may be in a deep length
 func (phpArr *PHPArray) AddDeepValue(paths []string, val *PHPValue) {
-    pathSize := len(paths)
-    refPhpArr := phpArr
-    if pathSize == 0 {
-        return
-    }
-    var refKVPair *PHPKeyValuePair
-    for i := 0; i < pathSize; i++ {
-        field := paths[i]
-        found := false
-        for _, v := range refPhpArr.Values {
-            if v.Key == field && v.Type == PhpTypeArray {
-                refPhpArr = v.Value.(*PHPArray)
-                found = true
-                if i == pathSize - 1 {
-                    refKVPair = v
-                }
-            }
-        }
-        if !found {
-            if i == pathSize - 1 {
-                kvPair := &PHPKeyValuePair{
-                    Key:field,
-                    Type:PhpTypeValue,
-                    Value:val,
-                }
-                refKVPair = kvPair
-                refPhpArr.Values = append(refPhpArr.Values, kvPair)
-            } else {
-                kvPair := &PHPKeyValuePair{
-                    Key:field,
-                    Type:PhpTypeArray,
-                    Value:NewPHPArray(),
-                }
-                refKVPair = kvPair
-                refPhpArr.Values = append(refPhpArr.Values, kvPair)
-                refPhpArr = refKVPair.Value.(*PHPArray)
-            }
-        } else {
-            if i == pathSize - 1 {
-                refKVPair.Type = PhpTypeValue
-                refKVPair.Value = val
-            }
-        }
-    }
+	pathSize := len(paths)
+	refPhpArr := phpArr
+	if pathSize == 0 {
+		return
+	}
+	var refKVPair *PHPKeyValuePair
+	for i := 0; i < pathSize; i++ {
+		field := paths[i]
+		found := false
+		for _, v := range refPhpArr.Values {
+			if v.Key == field && v.Type == PhpTypeArray {
+				refPhpArr = v.Value.(*PHPArray)
+				found = true
+				if i == pathSize-1 {
+					refKVPair = v
+				}
+			}
+		}
+		if !found {
+			if i == pathSize-1 {
+				kvPair := &PHPKeyValuePair{
+					Key:   field,
+					Type:  PhpTypeValue,
+					Value: val,
+				}
+				refKVPair = kvPair
+				refPhpArr.Values = append(refPhpArr.Values, kvPair)
+			} else {
+				kvPair := &PHPKeyValuePair{
+					Key:   field,
+					Type:  PhpTypeArray,
+					Value: NewPHPArray(),
+				}
+				refKVPair = kvPair
+				refPhpArr.Values = append(refPhpArr.Values, kvPair)
+				refPhpArr = refKVPair.Value.(*PHPArray)
+			}
+		} else {
+			if i == pathSize-1 {
+				refKVPair.Type = PhpTypeValue
+				refKVPair.Value = val
+			}
+		}
+	}
 }
 
 func (phpArr *PHPArray) AddChild(key string, val *PHPValue) {
-    kvPair := &PHPKeyValuePair{
-        Key:key,
-        Type:PhpTypeValue,
-        Value:val,
-    }
-    phpArr.Values = append(phpArr.Values, kvPair)
+	kvPair := &PHPKeyValuePair{
+		Key:   key,
+		Type:  PhpTypeValue,
+		Value: val,
+	}
+	phpArr.Values = append(phpArr.Values, kvPair)
 }
 
 func (phpArr *PHPArray) MergeChilds(arr *PHPArray) {
-    if arr == nil || len(arr.Values) == 0 {
-        return
-    }
-    for _, v := range arr.Values {
-        found := false
-        for _, ov := range phpArr.Values {
-            if v.Key == ov.Key {
-                ov.Value = v.Value
-                found = true
-                break
-            }
-        }
-        if !found {
-            phpArr.Values = append(phpArr.Values, v)
-        }
-    }
+	if arr == nil || len(arr.Values) == 0 {
+		return
+	}
+	for _, v := range arr.Values {
+		found := false
+		for _, ov := range phpArr.Values {
+			if v.Key == ov.Key {
+				ov.Value = v.Value
+				found = true
+				break
+			}
+		}
+		if !found {
+			phpArr.Values = append(phpArr.Values, v)
+		}
+	}
 }
 
 func (phpArr *PHPArray) String(depth int) string {
-    result := bytes.Buffer{}
-    result.WriteString(PHPArrayStartString)
-    if phpArr != nil {
-        valSize := len(phpArr.Values)
-        if phpArr.Values != nil && valSize > 0 {
-            result.WriteString("\n")
-            for i, kv := range phpArr.Values {
-                result.WriteString(IndentString)
-                result.WriteString(kv.String(depth + 1))
-                if i != valSize-1 {
-                    result.WriteString(",")
-                }
-                result.WriteString("\n")
-            }
-            result.WriteString(strings.Repeat(IndentString, depth+1))
-        }
-    }
-    result.WriteString(PHPArrayEndString)
-    return result.String()
+	result := bytes.Buffer{}
+	result.WriteString(PHPArrayStartString)
+	if phpArr != nil {
+		valSize := len(phpArr.Values)
+		if phpArr.Values != nil && valSize > 0 {
+			result.WriteString("\n")
+			for i, kv := range phpArr.Values {
+				result.WriteString(IndentString)
+				result.WriteString(kv.String(depth + 1))
+				if i != valSize-1 {
+					result.WriteString(",")
+				}
+				result.WriteString("\n")
+			}
+			result.WriteString(strings.Repeat(IndentString, depth+1))
+		}
+	}
+	result.WriteString(PHPArrayEndString)
+	return result.String()
 }
-
-
-
--- toml2php_test.go
+++ toml2php_test.go
@@ -1,137 +1,137 @@
 package toml2php
 
 import (
-    "io/ioutil"
-    "os"
-    "testing"
+	"io/ioutil"
+	"os"
+	"testing"
 )
 
 func TestNormalize(t *testing.T) {
-    tomlFile := "example.toml"
-    file, err := os.Open(tomlFile)
-    if err != nil {
-        t.Logf("open file %s failed \n", tomlFile)
-        t.Fail()
-    }
-    defer file.Close()
+	tomlFile := "example.toml"
+	file, err := os.Open(tomlFile)
+	if err != nil {
+		t.Logf("open file %s failed \n", tomlFile)
+		t.Fail()
+	}
+	defer file.Close()
 
-    tomlBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        t.Log("read file content failed\n")
-        t.Fail()
-    }
+	tomlBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Log("read file content failed\n")
+		t.Fail()
+	}
 
-    toml, err := normalize(string(tomlBytes))
-    if err != nil {
-        t.Logf("normalize toml failed: %s\n", err)
-        t.Fail()
-    }
+	toml, err := normalize(string(tomlBytes))
+	if err != nil {
+		t.Logf("normalize toml failed: %s\n", err)
+		t.Fail()
+	}
 
-    t.Log(toml)
+	t.Log(toml)
 }
 
 func TestParseArray(t *testing.T) {
-    tomlArr := `[ 'literal,', 'strings', 'quo"ted' ]`
-    tomlRunes := []rune(tomlArr)
-    parsed, err := parsePHPArray(tomlRunes)
-    if err != nil {
-        t.Logf("parsePHPArray failed: %s \n", err)
-        t.Fail()
-    }
-    t.Log("parsed: ", parsed.String(0))
+	tomlArr := `[ 'literal,', 'strings', 'quo"ted' ]`
+	tomlRunes := []rune(tomlArr)
+	parsed, err := parsePHPArray(tomlRunes)
+	if err != nil {
+		t.Logf("parsePHPArray failed: %s \n", err)
+		t.Fail()
+	}
+	t.Log("parsed: ", parsed.String(0))
 }
 
 func TestParseInlineTable(t *testing.T) {
-    tomlInlineTable := `PR17 = [
+	tomlInlineTable := `PR17 = [
   {title = "Home", url = "/", childs = []},
   {title = "Games", url = "/games", childs = [{title = "Game A", url = "/games/game-a", childs = []}, {title = "Game B", url = "/games/game-b", childs = []}]},
   {title = "About us", url = "/about", childs = []}
 ]`
-    parsed, err := parsePHPInlineTableFieldValue(tomlInlineTable)
-    if err != nil {
-        t.Logf("parsePHPInlineTable failed: %s \n", err)
-        t.Fail()
-        return
-    }
-    t.Log("parsed: ", parsed.String(0))
+	parsed, err := parsePHPInlineTableFieldValue(tomlInlineTable)
+	if err != nil {
+		t.Logf("parsePHPInlineTable failed: %s \n", err)
+		t.Fail()
+		return
+	}
+	t.Log("parsed: ", parsed.String(0))
 }
 
 func TestParsePHPKeyValue(t *testing.T) {
-    phpArr := &PHPArray{}
-    key := "PR17"
-    val := `[
+	phpArr := &PHPArray{}
+	key := "PR17"
+	val := `[
   {title = "Home", url = "/", childs = []},
   {title = "Games", url = "/games", childs = [{title = "Game A", url = "/games/game-a", childs = []}, {title = "Game B", url = "/games/game-b", childs = []}]},
   {title = "About us", url = "/about", childs = []}
 ]`
-    err := parsePHPKeyValue(phpArr, key, val)
-    if err != nil {
-        t.Logf("parsePHPKeyValue failed: %s \n", err)
-        t.Fail()
-        return
-    }
-    t.Log("parsed: ", phpArr.String(0))
+	err := parsePHPKeyValue(phpArr, key, val)
+	if err != nil {
+		t.Logf("parsePHPKeyValue failed: %s \n", err)
+		t.Fail()
+		return
+	}
+	t.Log("parsed: ", phpArr.String(0))
 }
 
 func TestParseMultiLine(t *testing.T) {
-    toml := `key3 = """
+	toml := `key3 = """
 One
 Two"""`
-    rs, err := parse(toml)
-    if err != nil {
-        t.Logf("parse %s failed: %s\n", toml, err)
-        t.Fail()
-        return
-    }
-    t.Logf("parse %s success: %s\n", toml, rs.String(0))
+	rs, err := parse(toml)
+	if err != nil {
+		t.Logf("parse %s failed: %s\n", toml, err)
+		t.Fail()
+		return
+	}
+	t.Logf("parse %s success: %s\n", toml, rs.String(0))
 }
 
 func TestParseSingle(t *testing.T) {
-    var tomls = []string{
-        // number
-        `123554`,
-        `23.4056`,
-        `0.2399`,
-        // boolean
-        `true`,
-        `false`,
-        // string
-        `"good"`,
-        `"hello,world"`,
-    }
-    for _, toml := range tomls {
-        rs, err := ParseSingle(toml)
-        if err != nil {
-            t.Logf("parse %s failed: %s\n", toml, err)
-            t.Fail()
-            continue
-        }
-        t.Logf("parse %s success: %s\n", toml, rs)
-    }
+	var tomls = []string{
+		// number
+		`123554`,
+		`23.4056`,
+		`0.2399`,
+		// boolean
+		`true`,
+		`false`,
+		// string
+		`"good"`,
+		`"hello,world"`,
+	}
+	for _, toml := range tomls {
+		rs, err := ParseSingle(toml)
+		if err != nil {
+			t.Logf("parse %s failed: %s\n", toml, err)
+			t.Fail()
+			continue
+		}
+		t.Logf("parse %s success: %s\n", toml, rs)
+	}
 }
 
 func TestParseTable(t *testing.T) {
-    tomlFile := "example.toml"
-    file, err := os.Open(tomlFile)
-    if err != nil {
-        t.Logf("open file %s failed \n", tomlFile)
-        t.Fail()
-    }
-    defer file.Close()
+	tomlFile := "example.toml"
+	file, err := os.Open(tomlFile)
+	if err != nil {
+		t.Logf("open file %s failed \n", tomlFile)
+		t.Fail()
+	}
+	defer file.Close()
 
-    tomlBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        t.Log("read file content failed\n")
-        t.Fail()
-    }
+	tomlBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Log("read file content failed\n")
+		t.Fail()
+	}
 
-    rs, err := ParseTable(string(tomlBytes))
-    if err != nil {
-        t.Logf("parse table failed: %s\n", err)
-        t.Fail()
-    }
+	rs, err := ParseTable(string(tomlBytes))
+	if err != nil {
+		t.Logf("parse table failed: %s\n", err)
+		t.Fail()
+	}
 
-    t.Logf("\n=======================\n")
+	t.Logf("\n=======================\n")
 
-    t.Log(rs)
+	t.Log(rs)
 }
